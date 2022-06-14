f4ede1a82a4d485622959c4b1601d89ae46348e3
Wenderson Bruno <wbrunobarroso@gmail.com>
2022-06-14 14:00:59 -0300

    fixing IMainNews interface

--- src/components/Templates/Home/MainNewsGroupSection/stories.tsx
+++ src/components/Templates/Home/MainNewsGroupSection/stories.tsx
@@ -11,18 +11,26 @@ export const Default: Story = (args) => (
     <MainNewsGroupSection
       mainNews={[
         {
-          titleText: 'exemple title',
-          descriptionText: 'descrição',
-          imgUrl:
+          title: 'exemple title',
+          summary: 'descrição',
+          image:
             'https://static.remove.bg/remove-bg-web/eb1bb48845c5007c3ec8d72ce7972fc8b76733b1/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg',
-          labelText: 'label 1',
+          chapeu: 'label 1',
+          url: '',
+          created: '',
+          id: '',
+          type: '',
         },
         {
-          titleText: 'exemple title 2',
-          descriptionText: 'descrição 2',
-          imgUrl:
+          title: 'exemple title 2',
+          summary: 'descrição 2',
+          image:
             'https://d5nunyagcicgy.cloudfront.net/external_assets/hero_examples/hair_beach_v391182663/original.jpeg',
-          labelText: 'label 2',
+          chapeu: 'label 2',
+          url: '',
+          created: '',
+          id: '',
+          type: '',
         },
       ]}
       {...args}
